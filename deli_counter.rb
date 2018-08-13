589c3350ad7965539006872e85a1d04e619fcec7
Thomas Pope <tpope0928@gmail.com>
2018-08-13 23:07:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,7 @@ end
 def take_a_number(katz_deli, name)
   katz_deli << name
   if katz_deli.length == 0
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.shift}."
-end
+   
 
 def now_serving
 end
